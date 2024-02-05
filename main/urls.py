7e83d67ac73933e4ccb5aa0702198452bc17dc70
khushi <khushi@gmail.com>
2024-02-05 17:37:52 +0530

    day2

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,10 @@ from.import views
 
 urlpatterns=[
     path("<int:id>",views.index,name="index"),
-    path("",views.home,name="home")
+    path("",views.home,name="home"),
+    path("create/", views.create, name="create")
 ]
+
 
 
 
